3093e6e90247b1a4ec8934b2f0e699f67cf9d953
Fostiriy <burakov.aa@students.dvfu.ru>
2023-01-05 16:55:47 +1000

    Добавить проверку областей видимости, добавить класс Variable для хранения информации о переменных

--- transpiler/semantixer/semantixer.py
+++ transpiler/semantixer/semantixer.py
@@ -1,7 +1,7 @@
 from nltk.tree import Tree
 
-from transpiler.base import TranspilerError
-from transpiler.constants import KEYWORDS, Label
+from transpiler.base import TranspilerError, Token
+from transpiler.constants import KEYWORDS, Label, FUNCTIONS
 
 
 class SemanticError(TranspilerError):
@@ -9,6 +9,8 @@ class SemanticError(TranspilerError):
 
 
 def is_correct_name(name):
+    if type(name) == Token:
+        name = name.value
     if name in KEYWORDS:
         return False
     return True
@@ -31,11 +33,7 @@ class SemanticAnalyzer:
 
     def is_correct(self, tree: Tree):
         result = True
-        for subtree in tree:
-            if subtree.label() == Label.FUNC_DECL:
-                self.__save_func(subtree)
-            elif subtree.label() == Label.MAIN_FUNC:
-                self.main_func = Function(subtree)
+        self.__find_func(tree)
 
         func_analyzer = FunctionAnalyzer(self.func_list)
         for func in self.func_list:
@@ -43,9 +41,19 @@ class SemanticAnalyzer:
 
         return result and func_analyzer.is_correct(self.main_func)
 
+    def __find_func(self, tree):
+        for subtree in tree:
+            if subtree.label() == Label.FUNC_DECL:
+                self.__save_func(subtree)
+                self.__find_func(subtree)
+            elif subtree.label() == Label.MAIN_FUNC:
+                self.main_func = Function(subtree)
+
     def __save_func(self, tree):
-        func = Function(tree)
-        self.func_list.append(func)
+        new_func = Function(tree)
+        for func in self.func_list:
+            assert new_func != func, f'Функция {new_func.id} уже была объявлена ранее'
+        self.func_list.append(new_func)
 
 
 class Function:
@@ -57,11 +65,12 @@ class Function:
         self.code = self.__find_code()
 
     def __find_id(self):
+        func_id = 'main'
         for subtree in self.tree:
             if subtree.label() == Label.ID:
                 func_id = subtree[0].value
                 assert is_correct_name(func_id), f'Использовано ключевое слово для идентификатора {func_id} {subtree}'
-                return func_id
+        return func_id
 
     def __find_type(self):
         for subtree in self.tree:
@@ -69,14 +78,15 @@ class Function:
                 return subtree[0][0]
 
     def __find_params(self, tree):
-        params = {}
+        params = []
         for subtree in tree:
             if subtree.label() == Label.FUNC_PARAMS:
-                param_id = subtree[1, 0].value
-                param_type = subtree[0, 0].value
+                param_id = subtree[1, 0]
+                param_type = subtree[0, 0]
                 assert is_correct_name(param_id), f'Использовано ключевое слово для идентификатора {param_id} {subtree}'
-                params[param_id] = param_type
-                return params | self.__find_params(subtree)
+                param = Variable(param_id, param_type)
+                params.append(param)
+                return params + self.__find_params(subtree)
         return params
 
     def __find_code(self):
@@ -88,24 +98,41 @@ class Function:
         if not isinstance(other, self.__class__):
             return False
         return self.id == other.id \
-            and self.type == other.type \
+            and self.type.value == other.type.value \
             and self.params == other.params
 
     def __str__(self):
         return f'Function: {self.id}\nType: {self.type}\nParameters: {self.params}\n'
 
 
+class Variable:
+    def __init__(self, var_id, var_type):
+        self.id = var_id
+        self.type = var_type
+        self.is_initialized = False
+
+    def init(self):
+        self.is_initialized = True
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.id.value == other.id.value \
+            and self.type.value == other.type.value
+
+    def __str__(self):
+        return f'Variable: {self.id.value}\nType: {self.type.value}\nInitialized: {self.is_initialized}'
+
+
 class FunctionAnalyzer:
     def __init__(self, func_list):
         # 0 for global scope variables
-        # 1 for nested if/for/while/until
-        # 2 for subnested and so on
+        # 1.. for nested if/for/while
         self.current_scope = 0
         self.func_list = func_list
         self.vars_dict = {}
 
     def is_correct(self, func: Function):
-        result = True
         self.vars_dict[self.current_scope] = func.params
         return self.__is_correct_code(func.code)
 
@@ -113,15 +140,69 @@ class FunctionAnalyzer:
         result = True
         for subtree in tree:
             if type(subtree) == Tree:
-                if subtree.label() == Label.VAR_DECL:
-                    var_id = subtree[1, 0]
-                    var_type = subtree[0, 0]
-                    assert is_correct_name(var_id), f'Использовано ключевое слово для идентификатора {var_id} {subtree}'
-                    self.__save_var(var_id, var_type)
+                match subtree.label():
+                    case Label.ASSIGNMENT:
+                        var_id = subtree[0]
+                        if var_id.label() != Label.ID:
+                            var_id = subtree[1, 0]
+                        else:
+                            var_id = subtree[0, 0]
+                        assert self.__is_var_exists(
+                            var_id), f'Переменная {var_id} не объявлена в окружении {self.current_scope}'
+
+                        # Проверка соответствия типов
+
+                    case Label.VAR_DECL:
+                        var_id = subtree[1, 0]
+                        var_type = subtree[0, 0]
+                        assert is_correct_name(
+                            var_id), f'Использовано ключевое слово для идентификатора {var_id} {subtree}'
+                        self.__save_var(var_id, var_type)
+
+                        for key, var_list in self.vars_dict.items():
+                            print(key)
+                            for var in var_list:
+                                print(var)
+
+                        # Проверка присваивания
+
+                    case Label.FUNC_CALL:
+                        func_id = subtree[0, 0].value
+                        functions = [func for func in self.func_list if func.id == func_id]
+                        assert functions != [] or func_id in FUNCTIONS, f'Функция {func_id} не объявлена'
+                        # func_params = self.__find_func_params(subtree)
+
+                        # Проверять аргументы и типы
+
+                    case Label.FUNC_RETURN:
+                        pass
+
+                    case Label.LBRACKET_CURLY:
+                        self.current_scope += 1
+                        self.vars_dict[self.current_scope] = {}
+
+                    case Label.RBRACKET_CURLY:
+                        self.vars_dict.pop(self.current_scope)
+                        self.current_scope -= 1
+
                 result = result and self.__is_correct_code(subtree)
         return result
 
-    def __save_var(self, var_type, var_id):
-        scoped_vars = self.vars_dict.get(self.current_scope)
-        scoped_vars[var_type] = var_id
-        self.vars_dict[self.current_scope] = scoped_vars
+    def __save_var(self, var_id, var_type):
+        var = Variable(var_id, var_type)
+        is_var_exists = False
+        for var_exists in self.vars_dict[self.current_scope]:
+            if var_exists == var:
+                is_var_exists = True
+        assert not is_var_exists, f'Переменная {var} уже объявлена'
+        self.vars_dict[self.current_scope].append(var)
+
+    def __is_var_exists(self, var_id):
+        for scope in range(0, self.current_scope + 1):
+            if var_id.value in [var_exists.id.value for var_exists in self.vars_dict[scope]]:
+                return True
+        return False
+
+    # def __find_func_params(self, tree):
+    #     for subtree in tree:
+    #         if subtree.label() == Label.FUNC_CALL_PARAMS:
